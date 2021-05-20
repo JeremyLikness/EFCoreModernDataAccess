807d090cdb9124531fcbd35e13d93cd485905a26
Jeremy Likness <jeremy@jeremylikness.com>
2021-05-20 09:01:45 -0700

    Add EF Core

---
+++ EFCoreModernDataAccess/DataAccess/ConferenceContext.cs
@@ -0,0 +1,39 @@
+﻿using EFModernDA.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFModernDA.DataAccess
+{
+    public class ConferenceContext : DbContext
+    {
+        public ConferenceContext(DbContextOptions<ConferenceContext> options) :
+            base(options)
+        {
+        }
+
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Session>()
+                .HasMany(s => s.Speakers)
+                .WithMany(sp => sp.Presentations);
+
+            modelBuilder.Entity<Session>()
+                .HasMany(s => s.Attendees)
+                .WithMany(a => a.Sessions);
+
+            modelBuilder.Entity<Tag>()
+                .HasMany(t => t.Sessions)
+                .WithMany(s => s.Tags);
+
+            modelBuilder.Entity<Tag>()
+                .HasMany(t => t.Speakers)
+                .WithMany(sp => sp.Tags);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- EFCoreModernDataAccess/Program.cs
+++ EFCoreModernDataAccess/Program.cs
@@ -1,4 +1,6 @@
-﻿using EFModernDA.Seed;
+﻿using EFModernDA.DataAccess;
+using EFModernDA.Seed;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 Console.WriteLine("Hello, DEVintersection!");
@@ -6,3 +8,24 @@ Console.WriteLine("Hello, DEVintersection!");
 // set up
 var sessions = Seeder.GetSessions();
 Console.WriteLine($"Loaded {sessions.Length} sessions.");
+
+// ======================================================================
+// database
+// note: for production apps, consider using migrations
+// https://docs.microsoft.com/ef/core/managing-schemas/migrations/
+// never use EnsureDeleted/Created for production, this is for demo only!
+
+var options = new DbContextOptionsBuilder<ConferenceContext>()
+    .UseSqlite("Data Source=conference.sqlite")
+    .Options;
+
+using var context = new ConferenceContext(options);
+context.Database.EnsureDeleted();
+context.Database.EnsureCreated();
+
+Console.WriteLine("Created the database. Now seeding the data...");
+
+context.AddRange(sessions);
+await context.SaveChangesAsync();
+
+Console.WriteLine("Database loaded.");
